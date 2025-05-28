7fed5a551e1592335d51db2423dbbd9a0753fb13
fransilandia <mafca21@gmail.com>
2025-05-28 15:20:05 -0400

    servidor funcionando

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// const express = require('express') // CJS Common JS
+import express from 'express' // ESM Ecmascript modules
+
+const app = express()
+
+// Routing
+
+app.get('/', (req, res) =>{
+    res.send('Hola Mundo en Express / TypeScript')
+}) 
+
+ const port = process.env.PORT ||  4000
+ 
+
+
+app.listen(port, () => {
+    console.log('Servidor Funcionanado en el puerto:', port)
+})
