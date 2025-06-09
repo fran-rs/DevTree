852fb2972778b814a18ed847be7aec9d47a36415
fransilandia <mafca21@gmail.com>
2025-06-09 10:55:30 -0400

    usanto postman

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 const router = Router()
 
-
+/** Autenticación y Registro **/
+router.post('/auth/register', (req, res) => {
+    console.log('Desde register')
+})
 
 export default router
