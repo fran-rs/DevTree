e7a1e3418063d03219909edfe1f866bc3e9afb80
fransilandia <mafca21@gmail.com>
2025-06-24 18:59:10 -0400

    aplicando type y interface

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,28 @@ import server from "./server"
 server.listen(port, () => {
     console.log( colors.cyan.bold( `Servidor Funcionanado en el puerto: ${port} `) )
 })
+
+
+let productName = "Tablet"
+let isAuth = false
+let price = 30
+
+interface Product  {
+    id: number
+    price: number
+    name: string
+}
+
+let product : Product = {
+    id: 1,
+    price: 30,
+    name: "Tablet"
+}
+
+let product2 : Product = {
+    id: 2,
+    price: 30,
+    name: "Tablet 11 pulgadas"
+}
+
+const numbers = [10, 20, 30]
